a356ef1d6bc370469b94f96024ad74b28b446304
akbarabustang <akbar_abustang@ymail.com>
2020-11-22 23:53:52 +0700

    templating

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,14 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     const data = {
-        greeting: 'Hello Fellas!'
+        image_profile: 'https://cdn.britannica.com/99/124299-050-4B4D509F/Linus-Torvalds-2012.jpg',
+        greeting: 'Hello, Welcome to my Portfolio Site',
+        languages: [
+            { name: 'javascript', year:10},
+            { name: 'php', year:10},
+            { name: 'phyton', year:10},
+            { name: 'ruby', year:10}
+        ]
     }
 
     res.render('landing', data)
